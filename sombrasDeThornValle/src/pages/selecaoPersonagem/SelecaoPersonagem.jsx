b1b01066b5670d7494139134ab3598604af66103
Bianco <arthnilo@gmail.com>
2024-09-09 03:19:11 -0300

    Feat: selectCharacter done

--- sombrasDeThornValle/src/pages/selecaoPersonagem/SelecaoPersonagem.jsx
+++ sombrasDeThornValle/src/pages/selecaoPersonagem/SelecaoPersonagem.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import style from './selecaoPersonagem.module.css'
+import personagem01 from '../../assets/characters/Protagonista01.png'
+import personagem02 from '../../assets/characters/Protagonista02.png'
 const SelecaoPersonagem = () => {
   return (
-    <div>
-      <h1 className={style.title}>Selecione seu personagem</h1>
+    <div className={style.selecaoContainer}>
+      <div className={style.containerInterno}>
+          <div className={style.containerInternoPersonagem}>
+            <img src={personagem01} alt="" />
+            <div className={style.containerInternoPersonagemInfo} >
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consectetur voluptatem modi </p>
+              <button  className={style.buttonSelection} >Escolher</button>
+            </div>
+          </div>
+      </div>
+      <div className={style.containerInterno}>
+      <div className={style.containerInternoPersonagem}>
+            <img src={personagem02} alt="" />
+            <div className={style.containerInternoPersonagemInfo} >
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consectetur voluptatem modi </p>
+              <button className={style.buttonSelection}>Escolher</button>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
