7450f51ac90e5fc8cae43115250e8a92988b4f2e
Bianco <arthnilo@gmail.com>
2024-10-04 14:56:21 -0300

    Feat:frondeLume Book

--- sombrasDeThornValle/src/pages/colheita/festaColheita.jsx
+++ sombrasDeThornValle/src/pages/colheita/festaColheita.jsx
@@ -3,7 +3,8 @@ import style from './festaColheita.module.css'
 import { useNavigate } from 'react-router-dom'
 import TextBackground from '../../components/textBackground/TextBackGroundComponent.jsx'
 import PlanilhaComponent from '../../components/planilhaCompInGame/planilhaComponent.jsx'
-import party01 from '../../assets/prologoImg/party01.png'
+import BookLumen from '../../assets/fragmentImage/bookCoverFrondeLume.png'
+import party01 from '../../assets/fragmentImage/FrondeLumeParty.png'
 import Elowen from '../../assets/characters/Elowen.png'
 import Helene from '../../assets/characters/Helene.png'
 import Berethur from '../../assets/characters/Berethur.png'
@@ -14,25 +15,26 @@ import Dorian from '../../assets/characters/Dorian.png'
 
 const festaColheita = () => {
   const navigate = useNavigate();
+  const [fadeIn, setFadeIn] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
+  const [openBookFrondeLume,  setOpenBookFrondeLume] = useState(false);
   const [isToggled, setIsToggled] = useState(false);
   const [isToggled2, setIsToggled2] = useState(false);
   const [openPopUp, setOpenPopUp] = useState(false);
-  const [fadeIn, setFadeIn] = useState(false);
-  const [fadeOut, setFadeOut] = useState(false);
-  const [window01, setWindow01] = useState(false);
-  const [window02, setWindow02] = useState(false);
-  const [window03, setWindow03] = useState(false);
-  const [window04, setWindow04] = useState(false);
-  const [window05, setWindow05] = useState(false);
-  const [window06, setWindow06] = useState(false);
-  const [window07, setWindow07] = useState(false);
-  const [window08, setWindow08] = useState(false);
   const [isToggled3, setIsToggled3] = useState(false);
   const [isToggled4, setIsToggled4] = useState(false);
   const [isToggled5, setIsToggled5] = useState(false);
   const [isToggled6, setIsToggled6] = useState(false);
   const [isToggled7, setIsToggled7] = useState(false);
   const [isToggled8, setIsToggled8] = useState(false);
+  const [window01, setWindow01] = useState(false);
+  const [elowen, setElowen] = useState(false);
+  const [freya, setFreya] = useState(false);
+  const [berethur, setBerethur] = useState(false);
+  const [helene, setHelene] = useState(false);  
+  const [faramond, setFaramond] = useState(false);
+  const [halstein , setHalstein] = useState(false);
+  const [dorian, setDorian] = useState(false);
 
   const handlePopUp = () => {
     setOpenPopUp(!openPopUp);
@@ -43,25 +45,141 @@ const festaColheita = () => {
     setWindow01(!window01);
     setFadeOut(!fadeOut);
     setIsToggled(!isToggled);
-    setWindow02(false);
+    setFreya(false);
     setIsToggled2(false);
     setIsToggled3(false);
+    setTimeout(() => {
+      setOpenBookFrondeLume(!openBookFrondeLume);
+    } , 200);
+  };
+
+  const bookFrondeLumeClose = () => {
+    setWindow01(false)
+    setOpenBookFrondeLume(false)
   };
 
   const windowOpen2 = () => {
-    setWindow02(!window02);
+    setElowen(!elowen);
     setFadeOut(!fadeOut);
     setIsToggled2(!isToggled2);
     setIsToggled3(false);
-    setWindow03(false);
+    setFreya(false);
+    setIsToggled4(false);
+    setIsToggled5(false);
+    setIsToggled6(false);
+    setIsToggled7(false);
+    setIsToggled8(false);
+    setBerethur(false);
+    setFaramond(false);
+    setHalstein(false);
+    setDorian(false);
+    setHelene(false);
+
   };
 
   const windowOpen3 = () => {
-    setWindow03(!window03);
+    setFreya(!freya);
     setFadeOut(!fadeOut);
     setIsToggled3(!isToggled3);
     setIsToggled2(false);
-    setWindow02(false);
+    setElowen(false);
+    setIsToggled4(false);
+    setIsToggled5(false);
+    setIsToggled6(false);
+    setIsToggled7(false);
+    setIsToggled8(false);
+    setBerethur(false);
+    setFaramond(false);
+    setHalstein(false);
+    setDorian(false);
+    setHelene(false);
+  };
+
+  const windowOpen4 = () => {
+    setBerethur(!berethur);
+    setFadeOut(!fadeOut);
+    setIsToggled4(!isToggled4);
+    setIsToggled5(false);
+    setIsToggled6(false);
+    setIsToggled7(false);
+    setIsToggled8(false);
+    setIsToggled2(false);
+    setIsToggled3(false);
+    setFreya(false);
+    setElowen(false);
+    setFaramond(false);
+    setHalstein(false);
+    setDorian(false);
+    setHelene(false);
+  };
+
+  const windowOpen5 = () => {
+    setHelene(!helene);
+    setFadeOut(!fadeOut);
+    setIsToggled5(!isToggled5);
+    setIsToggled4(false);
+    setIsToggled6(false);
+    setIsToggled7(false);
+    setIsToggled8(false);
+    setIsToggled2(false);
+    setIsToggled3(false);
+    setFreya(false);
+    setBerethur(false);
+    setElowen(false);
+    setFaramond(false);
+    setHalstein(false);
+    setDorian(false);
+  };
+
+  const windowOpen6 = () => {
+    setFaramond(!faramond);
+    setFadeOut(!fadeOut);
+    setIsToggled6(!isToggled6);
+    setIsToggled5(false);
+    setIsToggled4(false);
+    setIsToggled7(false);
+    setIsToggled8(false);
+    setIsToggled2(false);
+    setIsToggled3(false);
+    setFreya(false);
+    setBerethur(false);
+    setHelene(false);
+    setHalstein(false);
+    setDorian(false);
+  };
+
+  const windowOpen7 = () => {
+    setHalstein(!halstein);
+    setFadeOut(!fadeOut);
+    setIsToggled7(!isToggled7);
+    setIsToggled6(false);
+    setIsToggled5(false);
+    setIsToggled4(false);
+    setIsToggled8(false);    
+    setIsToggled2(false);
+    setIsToggled3(false);
+    setFreya(false);
+    setBerethur(false);
+    setHelene(false);
+    setFaramond(false);
+    setDorian(false);
+  };
+
+  const windowOpen8 = () => {
+    setDorian(!dorian);
+    setFadeOut(!fadeOut);
+    setIsToggled8(!isToggled8);
+    setIsToggled7(false);
+    setIsToggled6(false);
+    setIsToggled5(false);
+    setIsToggled4(false);
+    setIsToggled2(false);
+    setIsToggled3(false);
+    setFreya(false);
+    setBerethur(false);
+    setHelene(false);
+    setFaramond(false);
+    setHalstein(false);
   };
 
   return (
@@ -81,37 +199,71 @@ const festaColheita = () => {
     </div>
     <div className={style.containerImageBoard}>
       <div className={style.mapaContainer}>
-        <img onClick={windowOpen1} className={`${style.imgFlow} ${window01 ? style.fadeInSide : ''} ${isToggled ? style.toggleActive : ''}`}  src={party01} alt="" />
+        <img onClick={windowOpen1} className={`${style.imgFlow} ${window01 ? style.fadeOut : ''} ${isToggled ? style.toggleActive : ''}`}  src={BookLumen} alt="" />
         {window01 &&
           <div className={`${style.window01Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
             <img onClick={windowOpen2} className={`${style.imgPersonagens} ${isToggled2 ? style.toggleActive2 : ''}`} src={Elowen} alt=""/>
             <img onClick={windowOpen3} className={`${style.imgPersonagens} ${isToggled3 ? style.toggleActive3 : ''}`} src={Freya} alt=""/>
-            <img className={`${style.imgPersonagens} ${isToggled4 ? style.toggleActive2 : ''}`} src={Berethur} alt=""/>
-            <img className={`${style.imgPersonagens} ${isToggled5 ? style.toggleActive2 : ''}`} src={Helene} alt=""/>
-            <img className={`${style.imgPersonagens} ${isToggled6 ? style.toggleActive2 : ''}`} src={Faramond} alt=""/>
-            <img className={`${style.imgPersonagens} ${isToggled7 ? style.toggleActive2 : ''}`} src={Halstein} alt=""/>
-            <img className={`${style.imgPersonagens} ${isToggled8 ? style.toggleActive2 : ''}`} src={Dorian} alt=""/>
+            <img onClick={windowOpen4} className={`${style.imgPersonagens} ${isToggled4 ? style.toggleActive4 : ''}`} src={Berethur} alt=""/>
+            <img onClick={windowOpen5} className={`${style.imgPersonagens} ${isToggled5 ? style.toggleActive5 : ''}`} src={Helene} alt=""/>
+            <img onClick={windowOpen6} className={`${style.imgPersonagens} ${isToggled6 ? style.toggleActive6 : ''}`} src={Faramond} alt=""/>
+            <img onClick={windowOpen7} className={`${style.imgPersonagens} ${isToggled7 ? style.toggleActive7 : ''}`} src={Halstein} alt=""/>
+            <img onClick={windowOpen8} className={`${style.imgPersonagens} ${isToggled8 ? style.toggleActive8 : ''}`} src={Dorian} alt=""/>
           </div>
         }
-        {window02 &&
+        {elowen &&
           <div className={`${style.window02Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
             <img className={style.imgPersonagensIntern} src={Elowen} alt=""/>
             <h2>Elowen</h2>
-            <p>A jovem de 22 anos, é uma mulher com beleza marcante, com longos cabelos negros, lisos como seda, que caem suavemente sobre seus ombros. Sua pele é clara e suave, com um brilho natural que reflete sua juventude e vitalidade. Seus olhos brilhantes, de um castanho profundo, transmitem uma mistura de doçura e mistério. </p>
+            <p>Ela tem 22 anos e com seus cabelos negros como uma noite sem lua, desliza com graciosidade por onde passa. Seus fios lisos e longos caem sobre os ombros delicadamente, destacando sua pele clara e suave, quase como porcelana. Seus olhos, brilhantes e atentos, refletem uma intensidade que captura a atenção de todos à sua volta, transmitindo mistério e magnetismo. Sua beleza serena é irresistível, e há algo no seu sorriso discreto e no jeito como inclina a cabeça que cativa e atrai olhares, como se cada gesto fosse um convite velado. Com uma presença encantadora, Elowen parece carregar uma aura de sedução natural, exalando um charme hipnótico sem precisar dizer uma palavra. </p>
           </div>
         }
-        {window03 &&
+        {freya &&
           <div className={`${style.window03Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
             <img className={style.imgPersonagensIntern} src={Freya} alt=""/>
             <h2>Freya</h2>
             <p>A irmã mais nova de Elowen, compartilha da mesma beleza cativante. Ela tem 18 anos, com cabelos longos e negros, semelhantes aos de Elowen, sempre de cabelos presos. Sua pele clara e suave contrasta com seus olhos de um cinza pálido, sem visão desde o nascimento, mas sempre irradiando serenidade.Freya está constantemente sorrindo e é conhecida por sua atenção calorosa a todos ao seu redor. Sua ligação especial com a natureza é evidente; ela parece entender profundamente o ambiente que não pode ver, como se sentisse a vida ao seu redor em uma forma mais pura. </p>
           </div>
         }
-        {window04 &&
+        {berethur &&
           <div className={`${style.window04Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
             <img className={style.imgPersonagensIntern} src={Berethur} alt=""/>
             <h2>Berethur</h2>
-            <p>A irmã mais nova de Elowen, compartilha da mesma beleza cativante. Ela tem 18 anos, com cabelos longos e negros, semelhantes aos de Elowen, sempre de cabelos presos. Sua pele clara e suave contrasta com seus olhos de um cinza pálido, sem visão desde o nascimento, mas sempre irradiando serenidade.Freya está constantemente sorrindo e é conhecida por sua atenção calorosa a todos ao seu redor. Sua ligação especial com a natureza é evidente; ela parece entender profundamente o ambiente que não pode ver, como se sentisse a vida ao seu redor em uma forma mais pura. </p>
+            <p>Com 26 anos, ele é um homem robusto e de aparência média a alta, com cabelos curtos e castanhos, assim como os olhos. Sua inteligência não é seu ponto forte, mas sua lealdade e coragem são inigualáveis. Ele trabalha ao lado de seu avô no açougue dentro dos muros de ThornValle, e nunca perdeu uma Festa da Colheita. Sempre disposto a dar sua vida por uma amizade verdadeira, Berethur é conhecido por seus conselhos sinceros e sua disposição para ajudar, fazendo dele um companheiro confiável, mesmo nas situações mais difíceis.</p>
+          </div>
+        }
+        {helene &&
+          <div className={`${style.window05Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
+            <img className={style.imgPersonagensIntern} src={Helene} alt=""/>
+            <h2>Helene</h2>
+            <p>A mais velha dos três irmãos(27, 24 e 22), tem uma presença encantadora e intensa. Sua pele parda reluz sob a luz das tochas assim como seus cabelos cacheados, sempre soltos e esvoaçantes, enquadram um rosto delicado com olhos cor de mel profundo, que brilham com uma mistura de malícia e gentileza. Seus lábios carnudos curvam-se em sorrisos preguiçosos, mas quando ela toca seus instrumentos, especialmente a flauta, a preguiça desaparece, e a paixão aflora em cada nota.
+            Helene tem um coração generoso e nunca hesita em ajudar aqueles que ama. Todos ao seu redor sabem de seu afeto por Berethur, o tímido e leal amigo.</p>
+          </div>
+        }
+        {faramond &&
+          <div className={`${style.window05Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
+            <img className={style.imgPersonagensIntern} src={Faramond} alt=""/>
+            <h2>Faramond</h2>
+            <p>Um músico talentoso e charmoso, com cabelos cacheados castanhos e olhos cor de mel, herdados de sua irmã Helene. Ele leva uma vida indulgente, focada na música, no prazer e na luxúria. Sempre à procura de conforto e diversão, é raro vê-lo em FrondeLume, exceto em grandes festividades. Quando não está tocando, Faramond reclama da falta de vinho e das companhias femininas, sendo mais conhecido por sua busca incessante por prazeres do que por seu comprometimento com a vida cotidiana.</p>
+          </div>
+        }
+        {halstein &&
+          <div className={`${style.window05Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
+            <img className={style.imgPersonagensIntern} src={Halstein} alt=""/>
+            <h2>Halstein</h2>
+            <p>O irmão mais novo de Helene e Faramond, é um jovem de 21 anos que vive dentro dos muros de ThornValle e, ao contrário dos seus irmãos, tem pouca afinidade com música. Embora saiba tocar o tambor e seja habilidoso em manter o ritmo das canções festivas, ele prefere evitar apresentações e não compartilha da paixão artística da família. Prático, observador e tem um espírito inquieto, o que o faz desgostar de FrondeLume, achando a vila simples e tediosa. Sua verdadeira paixão esta em explorar a cidade de ThornValle, onde é fascinado pelas estruturas, torres e a vida dentro dos muros.</p>
+          </div>
+        }
+        {dorian &&
+          <div className={`${style.window05Container} ${window01 ? style.fadeIn : style.fadeOut}`}>
+            <img className={style.imgPersonagensIntern} src={Dorian} alt=""/>
+            <h2>Dorian</h2>
+            <p>Dorian é uma figura reclusa e enigmática, marcado pelo cheiro inconfundível de couro queimado, fruto de seu trabalho na forja da cidade. Apesar de estar entre seus amigos há seis meses, mantém uma postura distante e tímida, raramente se envolvendo nas interações sociais. Seu olhar atento e fixo revela uma fascinação por Freya, a quem observa discretamente, mas sem coragem de se aproximar. Ele prefere os cantos sombrios e é conhecido por sua pouca sociabilidade, carregando consigo um passado desconhecido que parece pesar sobre seu comportamento reservado e misterioso.</p>
+          </div>
+        }
+        {openBookFrondeLume && 
+          <div className={`${style.imgFlow2}`}>
+            <img onClick={bookFrondeLumeClose} src={party01} alt="" />
           </div>
         }
       </div>
